0978aea5da8e229a184c61583e4438945bcb0a3b
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-09-30 21:38:16 +0530

    mean value

---
+++ mean value .py
@@ -0,0 +1,9 @@
+a = int(input("enter the desired no.:"))
+b = int(input("enter the desired no.:"))
+c = int(input("enter the desired no.:"))
+d = int(input("enter the desired no.:"))
+e = (a+b+c+d)/4
+if (a+b+c+d)/4:
+    print ("the answer is",e)
+else:
+    print("invalid syntax")
