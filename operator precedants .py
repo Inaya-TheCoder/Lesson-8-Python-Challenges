55f5ae187505370a53512441b42a66911ff986c9
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-09-30 21:25:01 +0530

    precedants

--- operator precedants .py
+++ operator precedants .py
@@ -1,5 +1,7 @@
 a = int(input("enter the desired no.:"))
 b = int(input("enter the desired no.:"))
 c = int(input("enter the desired no.:"))
-d = a + b + c
-print ("the answer of a+b+c is", d)
+e = int(input("enter the desired no.:"))
+f = int(input("enter the desired no.:"))
+d = a+(b/c)*f-e 
+print ("the answer of a+(b/c)*f-e is", d)
