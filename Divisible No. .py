6917da259671c8154be9b4e55c2e66f6746456cd
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-09-30 21:30:51 +0530

    divsible no.

---
+++ Divisible No. .py
@@ -0,0 +1,6 @@
+a = int(input("enter the desired no.(numerator):"))
+b = int(input("enter the desired no. (denominator):"))
+if a%b==0:
+    print (a,"is divisible by",b)
+else:
+    print (a,"is not divisible by",b)
