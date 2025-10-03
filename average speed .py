781f8e7a262cd2b2ac3ee59a26de7448d739f363
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-10-03 20:24:41 +0530

    homework

---
+++ average speed .py
@@ -0,0 +1,7 @@
+a = int(input("enter the desired no.:"))
+b = int(input("enter the desired no.:"))
+c= int(input("enter the desired no.:"))
+d = int(input("enter the desired no.:"))
+e = (a+b+c+d)/4
+if :
+    print ()
---
+++ homework .py
@@ -0,0 +1,11 @@
+a = int(input("enter the no."))
+b = int(input("enter the no."))
+c = int(input("enter the no."))
+if a == (2):
+    print (a,"is equal to the description")
+elif b == (4):
+    print (b,"is equal to the description")
+elif c == (6):
+    print (c,"is equal to the description")
+else:
+    print ("no number describes the description")
